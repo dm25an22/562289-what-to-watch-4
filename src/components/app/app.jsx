46006045da85fe5b0a05c1bf33ce44ebe57a7498
Daniel <dm25an22@gmail.com>
2020-06-10 13:44:49 +0200

    module1-task3

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../main/main.jsx";
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {titlePromo, genrePromo, yearPromo} = props;
+  return (
+    <Main
+      titlePromo={titlePromo}
+      genrePromo={genrePromo}
+      yearPromo={yearPromo}
+    />
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  TITLE_PROMO: `The Grand Budapest Hotel`,
+  GENRE_PROMO: `Drama`,
+  YEAR_PROMO: 2014
+};
+
+ReactDom.render(
+    <App
+      titlePromo={Settings.TITLE_PROMO}
+      genrePromo={Settings.GENRE_PROMO}
+      yearPromo={Settings.YEAR_PROMO}
+    />,
+    document.querySelector(`#root`)
+);
