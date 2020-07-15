868107d36e2801295c50e184fb0e19c4eb9e9d86
Daniel <dm25an22@gmail.com>
2020-07-15 17:15:16 +0200

    module7-task2

--- src/components/movie-details/movie-details.test.js
+++ src/components/movie-details/movie-details.test.js
@@ -2,12 +2,36 @@ import React from "react";
 import renderer from "react-test-renderer";
 import MovieDetails from "./movie-details.jsx";
 import {mockFilms} from "../../mocks/mock-for-tests";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import NameSpace from "../../reducer/name-space";
+
+const mockStore = configureStore({});
+
+const AuthorizationStatus = {
+  NO_AUTH: `NO_AUTH`,
+  AUTH: `AUTH`
+};
+
+const store = mockStore({
+  [NameSpace.DATA]: {
+    films: mockFilms,
+  },
+  [NameSpace.APP_STATE]: {
+    currentGenre: `All genre`,
+  },
+  [NameSpace.USER]: {
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  }
+});
 
 it(`Render MoviePage`, () => {
   const tree = renderer.create(
-      <MovieDetails
-        film={mockFilms[0]}
-      />
+      <Provider store={store}>
+        <MovieDetails
+          film={mockFilms[0]}
+        />
+      </Provider>
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,69 @@
+import React, {createRef} from "react";
+import PropTypes from "prop-types";
+import Footer from "../footer/footer.jsx";
+
+class SignIn extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._loginRef = createRef();
+    this._passwordRef = createRef();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    const {onSubmit} = this.props;
+
+    onSubmit({
+      email: this._loginRef.current.value,
+      password: this._passwordRef.current.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
+
+        <div className="sign-in user-page__content">
+          <form onSubmit={this.handleSubmit} action="#" className="sign-in__form">
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input ref={this._loginRef} className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" required/>
+                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              </div>
+              <div className="sign-in__field">
+                <input ref={this._passwordRef} className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" required/>
+                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              </div>
+            </div>
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">Sign in</button>
+            </div>
+          </form>
+        </div>
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+SignIn.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default SignIn;
